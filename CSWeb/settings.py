fe5c20c648e3585aa8156918b1a5839d45f9ba41
Eagle1191 <Werise92@gmail.com>
2017-04-24 17:06:28 -0700

    updating for heroku

--- CSWeb/settings.py
+++ CSWeb/settings.py
@@ -23,8 +23,6 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, '/tether/templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'tether/static')
 
-MEDIA_DIR = os.path.join(BASE_DIR, 'tether/media')
-
 #FIXTURE_DIRS = os.path.join(BASE_DIR, 'tether/fixtures')
 
 AUTH_PROFILE_MODULE = "tether.UserProfile"
@@ -137,11 +135,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = (
     STATIC_DIR,
