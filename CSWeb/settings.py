f672b8e5405cab2525430ff59015ec211bd92900
Eagle1191 <Werise92@gmail.com>
2017-04-24 17:54:15 -0700

    updating for heroku

--- CSWeb/settings.py
+++ CSWeb/settings.py
@@ -141,8 +141,9 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    STATIC_DIR,
+    os.path.join(BASE_DIR, '/tether/static'),
 )
 
 # Simplified static file serving.
